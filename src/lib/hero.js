d3177189c63259f321442687e6ec274c9d805ed9
dehcanuto <andrecanutoc@gmail.com>
2021-11-22 14:35:02 -0300

    2 files modified

--- src/lib/hero.js
+++ src/lib/hero.js
@@ -1,19 +1,9 @@
 import api from '../services/api';
 
-const timestamp = '1637599932022'; // new Date().getTime();
-const AcessToken = '7675892e018b4cd58114f8bcc9f4ee08';
-const hashToken = '360c0534c98a99247ad8f2f7cf0239d3';
-
 export async function listHeros() {
-  const response = await api.get(`/characters`, {
-    params: {
-      apikey: AcessToken,
-      hash: hashToken,
-      ts: timestamp,
-    },
-  });
-  if (response.status !== 200) throw new Error('Erro ao exibir projetos');
-  return response.data.objects;
+  const response = await api.get(`/characters`);
+  if (response.code !== 200) throw new Error('Erro ao exibir herois');
+  return response.data;
 }
 
 export async function getHero(id) {
--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: `https://gateway.marvel.com/v1/public`,
+  params: {
+    apikey: '7675892e018b4cd58114f8bcc9f4ee08',
+    hash: '360c0534c98a99247ad8f2f7cf0239d3',
+    ts: '1637599932022',
+  },
 });
 
 api.interceptors.request.use((error) => {
