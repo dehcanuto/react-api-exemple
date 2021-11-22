4531ee63c2870dcc38b0179d11190caea2c2a950
dehcanuto <andrecanutoc@gmail.com>
2021-11-22 15:58:53 -0300

    1 file modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,15 +9,15 @@ export default function HomePage() {
   const [heroes, setHeroes] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    async function init() {
-      console.log('foi 2');
-      setHeroes(await listHeros());
-      setLoading(false);
-      console.log('heroes', heroes);
-    }
-    init();
-  }, [heroes]);
+  // useEffect(() => {
+  //   async function init() {
+  //     console.log('foi 2');
+  //     setHeroes(await listHeros());
+  //     setLoading(false);
+  //     console.log('heroes', heroes);
+  //   }
+  //   init();
+  // }, [heroes]);
 
   return (
     <div className="min-w-screen min-h-screen bg-gray-200 flex items-center justify-center px-5 py-5">
