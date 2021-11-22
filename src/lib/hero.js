a3813a46ecae82abadaec72b2ac802448afdee15
dehcanuto <andrecanutoc@gmail.com>
2021-11-22 15:22:52 -0300

    1 file modified

--- src/lib/hero.js
+++ src/lib/hero.js
@@ -2,12 +2,13 @@ import api from '../services/api';
 
 export async function listHeros() {
   const response = await api.get(`/characters`);
+  console.log('response', response);
   if (response.code !== 200) throw new Error('Erro ao exibir herois');
   return response.data;
 }
 
-export async function getHero(id) {
-  const response = await api.get(`/${id}`);
-  if (response.status !== 200) throw new Error('Erro ao exibir projetos');
-  return response.data.objects;
-}
+// export async function getHero(id) {
+//   const response = await api.get(`/${id}`);
+//   if (response.status !== 200) throw new Error('Erro ao exibir projetos');
+//   return response.data.objects;
+// }
